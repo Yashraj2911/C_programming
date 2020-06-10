f23c83c9f667be2f0e65a92cb7bf5dc707b75f93
Coursera Learner <nobody@nowhere.nul>
2020-06-10 13:35:19 +0000

    Did assignment 03_code2

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -1,7 +1,7 @@
 
 int printTriangle(int size) {
   int starcount=0;//start with starCount being 0
-  for(int i=0;i<size:i++)
+  for(int i=0;i<size;i++)
   {
     for(int j=0;j<=i;j++)
       printf("*"),starcount++;

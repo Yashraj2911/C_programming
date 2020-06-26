27cbb75f42b54c85c3aede9983c8d7bb64846be7
Coursera Learner <nobody@nowhere.nul>
2020-06-26 11:44:37 +0000

    DId 15

--- 15_tests_subseq/test-subseq.c
+++ 15_tests_subseq/test-subseq.c
@@ -3,7 +3,7 @@ size_t maxSeq(int* array,size_t n);
 int main()
 {
   int arr1[]={1,2,2,3,4,5}; 
-  if( maxSeq(arr1,6)!=4);
+  if( maxSeq(arr1,6)!=4)
   return EXIT_FAILURE;
   int arr2[]={0,1,-1};
   if(maxSeq(arr2,3)!=2)

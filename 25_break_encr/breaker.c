700a16866e2004f6d5eef57899e9cd4b65c55a5a
Coursera Learner <nobody@nowhere.nul>
2020-07-11 11:17:42 +0000

    did 25

---
+++ 25_break_encr/breaker.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+int main(int argc,char* argv[])
+{
+  if(argc!=2)
+    return EXIT_FAILURE;
+  FILE* f=fopen(argv[1],"r");
+  int c;
+  int arr[26]={0};
+  while((c=fgetc(f))!=EOF)
+    {
+      if(!isalpha(c))
+	continue;
+      c=tolower(c);
+      arr[c-97]++;
+    }
+  int max=0;
+  for(int i=0;i<26;i++)
+    if(max<arr[i])
+      max=i;
+  return max;
+}

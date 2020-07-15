692efb9e99352d90827bc7164d231b0f0cfdf9d1
Coursera Learner <nobody@nowhere.nul>
2020-07-15 11:33:43 +0000

    Did 27

---
+++ 27_matrix_input/rotateMatrix.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+void rotate(char matrix[10][10])
+{
+   char temp[10][10];
+  for(int i=0;i<10;i++)
+    for(int j=0;j<10;j++)
+      temp[i][j]=matrix[i][j];
+
+  int sum=9;
+  for(int i=0;i<10;i++)
+    {
+      for(int j=0;j<10;j++)
+	{
+	  int row_sum=sum+i+j;
+	  matrix[j][row_sum-j]=temp[i][j];
+	}
+      sum-=2;
+    }
+}
+void printMatrix(char matrix[10][10])
+{
+  for(int i=0;i<10;i++)
+    {
+      for(int j=0;j<10;j++)
+	printf("%c",matrix[i][j]);
+      printf("\n");
+    }
+}
+int main(int argc,char* argv[])
+{
+  if(argc!=2)
+    {
+      fprintf(stderr,"\nToo few or many arguments");
+      return EXIT_FAILURE;
+    }
+  FILE* f=fopen(argv[1],"r");
+  if(!f)
+    {
+      fprintf(stderr,"\nFile does not exist..");
+      return EXIT_FAILURE;
+    }
+  char matrix[10][10];
+  int c,i=1,j=0;
+  while((c=fgetc(f))!=EOF)
+    {
+      if(c=='\n'&& !(i%11))
+	{
+	  i=1;
+	  j++;
+	  continue;
+	}
+      if(c=='\n' && i%11)
+	{
+	  fprintf(stderr,"\nInvalid input..");
+	  return EXIT_FAILURE;
+	}
+      matrix[j][i-1]=c;
+      i++;
+    }
+  rotate(matrix);
+  printMatrix(matrix);
+  if(!fclose(f))
+    return EXIT_SUCCESS;
+  else
+    {
+      fprintf(stderr,"\nFile was not closed successfully..");
+      return EXIT_FAILURE;
+    }
+}

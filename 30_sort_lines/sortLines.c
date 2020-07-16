7420d0d1185f785148947a4a3188bece9a8469a6
Coursera Learner <nobody@nowhere.nul>
2020-07-16 13:42:30 +0000

    Did 30

--- 30_sort_lines/sortLines.c
+++ 30_sort_lines/sortLines.c
@@ -15,10 +15,46 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
-
+void printData(char **arr)
+{
+  int i=0;
+      while(arr[i])
+	{
+	  printf("%s\n",arr[i]);
+	  free(arr[i++]);
+	}
+      free(arr);
+}
+void read(FILE* stream)
+{
+  char* line=NULL;
+      char** arr;
+      size_t size=0;
+      size_t len,i=0;
+      while((len=getline(&line,&size,stream))>=0)
+	{
+	  arr=realloc(arr,(i+1)*sizeof(*arr));
+	  arr[i++]=line;
+	  line=NULL;
+	}
+      sortData(arr,i+1);
+      printData(arr);
+}
 int main(int argc, char ** argv) {
   
-  //WRITE YOUR CODE HERE!
-  
+  if(argc==1)
+    {
+      read(stdin);
+    }
+  else
+    {
+      FILE* f=fopen(argv[1],"r");
+      if(!f)
+	{
+	  fprintf(stderr,"\nFile does not exist..");
+	  return EXIT_FAILURE;
+	}
+      read(f);
+    }
   return EXIT_SUCCESS;
 }

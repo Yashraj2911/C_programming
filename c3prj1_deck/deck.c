f2bc97703b322678f9d6296a172d5000d78819af
Coursera Learner <nobody@nowhere.nul>
2020-07-29 15:08:51 +0000

    Did prj1

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -31,37 +31,37 @@ void assert_full_deck(deck_t * d) {
   for(int i=0;i<d->n_cards;i++)
     assert_card_valid(*d->cards[i]);
 }
-void add_card_to(dect_t* deck,card_t c)
+void add_card_to(deck_t* deck,card_t c)
 {
-  deck->cards=realloc(deck->cards,(n_cards+1)*sizeof(*deck->cards));
-  deck->cards[n_cards]=malloc(sizeof(card_t));
-  deck->cards[n_cards]->value=c.value;
-  deck->cards[n_cards]->suit=c.suit;
+  deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+  deck->cards[deck->n_cards]=malloc(sizeof(card_t));
+  deck->cards[deck->n_cards]->value=c.value;
+  deck->cards[deck->n_cards]->suit=c.suit;
   deck->n_cards++;
 }
 card_t* add_empty_card(deck_t* deck)
 {
-  deck->cards=realloc(deck->cards,(n_cards+1)*sizeof(*deck->cards));
-  deck->cards[n_cards]=malloc(sizeof(card_t));
-  deck->cards[n_cards]->value=0;
-  deck->cards[n_cards]->suit=0;
+  deck->cards=realloc(deck->cards,(deck->n_cards+1)*sizeof(*deck->cards));
+  deck->cards[deck->n_cards]=malloc(sizeof(card_t));
+  deck->cards[deck->n_cards]->value=0;
+  deck->cards[deck->n_cards]->suit=0;
   deck->n_cards++;
-  return deck->cards[n_cards-1];
+  return deck->cards[deck->n_cards-1];
 }
 deck_t* make_deck_exclude(deck_t* excluded_cards)
 {
   deck_t* excl=malloc(sizeof(deck_t));
-  excl->arr=NULL;
+  excl->cards=NULL;
   excl->n_cards=0;
   for(int i=0;i<52;i++)
     {
       card_t temp=card_from_num(i);
       if(!deck_contains(excluded_cards,temp))
 	{
-	  excl->arr=realloc(excl->arr,(n_cards+1)*sizeof(*excl->arr));
-	  excl->arr[n_cards]=malloc(sizeof(card_t));
-	  excl->arr[n_cards]->value=temp.value;
-	  excl->arr[n_cards]->suit=temp.suit;
+	  excl->cards=realloc(excl->cards,(deck->n_cards+1)*sizeof(*excl->cards));
+	  excl->cards[excl->n_cards]=malloc(sizeof(card_t));
+	  excl->cards[excl->n_cards]->value=temp.value;
+	  excl->cards[excl->n_cards]->suit=temp.suit;
 	  excl->n_cards++;
 	}
     }
@@ -71,14 +71,14 @@ deck_t* build_remaining_deck(deck_t** hands,size_t n_hands)
 {
   deck_t* excluded_deck=malloc(sizeof(deck_t));
   excluded_deck->n_cards=0;
-  excluded_deck->arr=NULL;
+  excluded_deck->cards=NULL;
   for(int i=0;i<n_hands;i++)
     {
       for(int j=0;j<hands[i]->n_cards;j++)
 	{
-	  if(hands[i]->arr[j]->value||hands[i]->arr[j]->suit)
+	  if(hands[i]->cards[j]->value||hands[i]->cards[j]->suit)
 	    {
-	      add_card_to(excluded_deck,*hands[i]->arr[j]);
+	      add_card_to(excluded_deck,*hands[i]->cards[j]);
 	    }
 	}
     }

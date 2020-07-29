15f174922b375cd634e504f612e573475422f0a7
Coursera Learner <nobody@nowhere.nul>
2020-07-29 08:25:17 +0000

    Did 34

--- 34_put_together/main.c
+++ 34_put_together/main.c
@@ -25,6 +25,8 @@ counts_t * countFile(const char * filename, kvarray_t * kvPairs) {
 
 int main(int argc, char ** argv) {
   //WRITE ME (plus add appropriate error checking!)
+  if(argc<3)
+    return EXIT_FAILURE;
   kvarray_t* kv=readKVs(argv[1]); //read the key/value pairs from the file named by argv[1] (call the result kv)
   if(!kv)
     {

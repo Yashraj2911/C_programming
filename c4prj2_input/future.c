98a6fd825bfdb6a6ef674507a815ac44b42a1ae6
Coursera Learner <nobody@nowhere.nul>
2020-07-31 10:49:03 +0000

    Did c4prj2

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -21,17 +21,17 @@ void add_future_card(future_cards_t* fc,size_t index,card_t* ptr)
 }
 void future_cards_from_deck(deck_t* deck,future_cards_t* fc)
 {
-  if(deck.n_cards!=fc->n_decks)
+  if(deck->n_cards!=fc->n_decks)
     {
-      fprintf(stderr,"\nNot enough cards")
+      fprintf(stderr,"\nNot enough cards");
 	return;
     }
   for(int i=0;i<fc->n_decks;i++)
     {
       for(int j=0;j<fc->decks[i].n_cards;j++)
 	{
-	  fc->decks[i].cards[j]->value=deck.cards[i]->value;
-	  fc->decks[i].cards[j]->suit=deck.cards[i]->suit;
+	  fc->decks[i].cards[j]->value=deck->cards[i]->value;
+	  fc->decks[i].cards[j]->suit=deck->cards[i]->suit;
 	}
     }
 }

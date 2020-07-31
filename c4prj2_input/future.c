4232d237710995b9a84a8d925555cf32a8be0bb6
Coursera Learner <nobody@nowhere.nul>
2020-07-31 08:13:28 +0000

    Did c4prj2

---
+++ c4prj2_input/future.c
@@ -0,0 +1,32 @@
+#include "future.h"
+void add_future_card(future_cards_t* fc,size_t index,card_t* ptr)
+{
+  if(index>=fc->n_decks)
+    {
+      fc->decks=realloc(fc->decks,(index+1)*sizeof(*fc));
+      fc->decks[index]->n_cards=0;
+      fc->decks[index]->cards[fc->n_cards++]=ptr;
+      fc->n_decks++;
+    }
+  else
+    {
+      fc->decks[index]->cards[fc->decks[index]->n_cards-1]=ptr;
+      fc->decks[index]->n_cards++;
+    }
+}
+void future_cards_from_deck(deck_t* deck,future_cards_t* fc)
+{
+  if(deck->n_cards!=fc->n_decks)
+    {
+      fprintf(stderr,"\nNot enough cards")
+	return;
+    }
+  for(int i=0;i<fc->n_decks;i++)
+    {
+      for(int j=0;j<fc->decks[i]->n_cards;j++)
+	{
+	  fc->decks[i]->cards[j]->value=deck->cards[i]->value;
+	  fc->decks[i]->cards[j]->suit=deck->cards[i]->suit;
+	}
+    }
+}
---
+++ c4prj2_input/input.c
@@ -0,0 +1,43 @@
+deck_t** read_input(FILE* f,size_t* n_hands,future_cards_t* fc)
+{
+  deck_t** answer=NULL;
+  size_t size;
+  int len=0,i=0;
+  char* line=NULL;
+  while((len=getline(&line,&size,f))>=0)
+    {
+      if(line[len-1]=='\n')
+	{
+	  line[len-1]=0;
+	  len--;
+	}
+      answer=realloc(answer,(i+1)*sizeof(*answer));
+      answer[i]=malloc(sizeof(deck_t));
+      answer[i]->cards=NULL;
+      answer[i]->n_cards=0;
+      char* line_ind=line;
+      int count=0;
+      while(line_ind)
+	{
+	  if(*line_ind==32)
+	    line++;
+	  if(*line_ind=='?')
+	    {
+	      add_future_card(fc,*(++line_ind)-48,add_empty_card(answer[i]););
+	    }
+	  else
+	    add_card_to(answer[i],card_from_letters(*line_ind,*(++line_ind)));
+	  count++;
+	}
+      if(count<5)
+	{
+	  fprintf(stderr,"\nInvalid input");
+	  exit(EXIT_FIALURE);
+	}
+      i++;
+      free(line);
+      line=line_ind=NULL;
+    }
+  *n_hands=i;
+  return answer;
+}

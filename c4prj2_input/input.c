a2c706df4192a20023fa5ec1d67ee0a5d7c206a3
Coursera Learner <nobody@nowhere.nul>
2020-08-01 12:20:13 +0000

    DID c4prj2

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -33,7 +33,7 @@ deck_t** read_input(FILE* f,size_t* n_hands,future_cards_t* fc)
 	    }
 	  else
 	    {
-	      char first=*line;
+	      char first=*line_ind;
 	      if(!isdigit(first)||(first!='K'&&first!='A'&&first!='Q'&&first!='J'))
 		{
 		  fprintf(stderr,"\nInvalid input 1");

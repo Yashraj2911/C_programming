bc75aabb842fda73973b8f36a236fa417ffa28ec
Coursera Learner <nobody@nowhere.nul>
2020-08-02 08:43:27 +0000

    DID

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -23,6 +23,10 @@ void add_future_card(future_cards_t* fc,size_t index,card_t* ptr)
 }
 void future_cards_from_deck(deck_t* deck,future_cards_t* fc)
 {
+  if(!fc)
+    {
+      return;
+    }
   if(deck->n_cards!=fc->n_decks)
     {
       fprintf(stderr,"\nNot enough cards");

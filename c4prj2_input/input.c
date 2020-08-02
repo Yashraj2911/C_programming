3945ebd4ed143972d0af9b8a709b4441009b1974
Coursera Learner <nobody@nowhere.nul>
2020-08-02 07:54:54 +0000

    DID

--- c4prj2_input/input.c
+++ c4prj2_input/input.c
@@ -6,6 +6,11 @@
 #include <ctype.h>
 deck_t** read_input(FILE* f,size_t* n_hands,future_cards_t* fc)
 {
+  if(!f)
+    {
+      fprintf(stderr,"\nnvalid file");
+      exit(EXIT_FAILURE);
+    }
   deck_t** answer=NULL;
   size_t size;
   int len=0,i=0;

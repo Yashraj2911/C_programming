8ff9b79313d7a5bcdce38545971347869c4317fb
Coursera Learner <nobody@nowhere.nul>
2020-08-03 09:32:33 +0000

    D

--- c4prj2_input/future.c
+++ c4prj2_input/future.c
@@ -17,7 +17,7 @@ void add_future_card(future_cards_t* fc,size_t index,card_t* ptr)
       fc->decks=realloc(fc->decks,(index+1)*sizeof(*fc->decks));
       for(int i=fc->n_decks;i<index;i++)
 	{
-	  fc->decks[i]=malloc(sizeof(deck_t))
+	  fc->decks[i]=malloc(sizeof(deck_t));
 	  fc->decks[i].cards=NULL;
 	  fc->decks[i].n_cards=0;
 	}

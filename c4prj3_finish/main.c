01553fd6e44d01c14b1f839c1f186838c4164efb
Coursera Learner <nobody@nowhere.nul>
2020-08-29 08:04:18 +0000

    Last

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -25,13 +25,13 @@ int main(int argc, char ** argv) {
     }
   
   size_t n_hands;
-  future_cards_t* fc=malloc(sizeof(*fc));
-  fc->decks=malloc(sizeof(deck_t));
-  fc->decks[0].cards=NULL;
-  fc->decks[0].n_cards=0;
-  fc->n_decks=1;
+  future_cards_t fc;//=malloc(sizeof(*fc));
+  fc.decks=malloc(sizeof(deck_t));
+  fc.decks[0].cards=NULL;
+  fc.decks[0].n_cards=0;
+  fc.n_decks=1;
 
-  deck_t** input=read_input(f,&n_hands,fc);
+  deck_t** input=read_input(f,&n_hands,&fc);
   deck_t* rem_deck=build_remaining_deck(input,n_hands);
   unsigned int* win=calloc((n_hands+1),sizeof(*win));
   unsigned int num_trials;
@@ -46,7 +46,7 @@ int main(int argc, char ** argv) {
   for(unsigned int i=0;i<num_trials;i++)
     {
 	shuffle(rem_deck);
-      future_cards_from_deck(rem_deck,fc);
+      future_cards_from_deck(rem_deck,&fc);
       int decide;
       deck_t *highest=input[0];
       int index=0;
@@ -70,15 +70,15 @@ int main(int argc, char ** argv) {
 	}
       printf("And there were %u ties\n",win[n_hands]);
     }
-   for(int i=0;i<fc->n_decks;i++)
+   for(int i=0;i<fc.n_decks;i++)
    {
      // for(int j=0;j<fc->decks[i].n_cards;j++)
      //	free(fc->decks[i].cards[j]);
      //     if(fc->decks[i].cards)
-       free(fc->decks[i].cards);
+       free(fc.decks[i].cards);
    }
-  free(fc->decks);
-  free(fc);
+  free(fc.decks);
+  //  free(fc);
   for(int i=0;i<n_hands;i++)
     free_deck(input[i]);
   free(input);
